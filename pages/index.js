aaf9e20d693ef59d6e0082f6c5a6ed61c4188921
Efendi Varol <efendiv@icloud.com>
2023-03-29 10:37:00 +0200

    changed reviewed things

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import QuizForm from "../components/QuizCardPage/QuizPageForm";
 export default function Home() {
   return (
     <main>
-      <QuizForm></QuizForm>
+      <QuizForm />
     </main>
   );
 }
